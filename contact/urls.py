5659364b4fba09e3e2512d11404370a5381df4aa
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-03-07 17:03:43 -0500

    Contact app modified

--- contact/urls.py
+++ contact/urls.py
@@ -4,9 +4,8 @@ from . import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.contact_list, name='contact_list'),
+    path('', views.contact_create, name='contact_create'),
     path('<int:pk>/', views.contact_detail, name='contact_detail'),
-    path('create/', views.contact_create, name='contact_create'),
     path('<int:pk>/update/', views.contact_update, name='contact_update'),
     path('<int:pk>/delete/', views.contact_delete, name='contact_delete'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -4,13 +4,11 @@ from .forms import ContactForm
 from django.core.mail import send_mail
 from django.conf import settings
 
-def contact_list(request):
-    contacts = Contact.objects.all()
-    return render(request, 'contact/contact_list.html', {'contacts': contacts})
 
 def contact_detail(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
     return render(request, 'contact/contact_detail.html', {'contact': contact})
+
 
 def contact_create(request):
     if request.method == 'POST':
@@ -19,16 +17,17 @@ def contact_create(request):
             contact = form.save()
             # Envoyer un e-mail (exemple)
             send_mail(
-                'Nouveau message de contact',
-                f'Vous avez reçu un nouveau message de {contact.full_name}. Sujet: {contact.subject}',
+                'Nouveau message de contact from your portfolio',
+                f"Vous avez reçu un nouveau message de {contact.full_name}. \nEmail: {contact.email}. \nSujet: {contact.subject}. \nMessage: {contact.message}",
                 settings.EMAIL_HOST_USER,
                 [settings.EMAIL_HOST_USER],
                 fail_silently=True,
             )
-            return redirect('contact:contact_list')
+            return redirect('contact:contact_detail', pk=contact.id)
     else:
         form = ContactForm()
     return render(request, 'contact/contact_form.html', {'form': form})
+
 
 def contact_update(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
@@ -40,6 +39,7 @@ def contact_update(request, pk):
     else:
         form = ContactForm(instance=contact)
     return render(request, 'contact/contact_form.html', {'form': form})
+
 
 def contact_delete(request, pk):
     contact = get_object_or_404(Contact, pk=pk)
