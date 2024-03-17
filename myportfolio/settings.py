35dd2ff9c22414b9a0f0bb2c1e5f27167fc71f2e
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-03-17 17:17:46 -0400

    decouple removed

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+# from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = BASE_DIR / '.env'
@@ -21,7 +21,7 @@ dotenv_path = BASE_DIR / '.env'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.path.join('SECRET_KEY')
 
 # 'django-insecure-5j*s4a^z+0%4fq7^z0fr8fnlxq-ckbz1r#*ki3w7c*9$8t7j5b'
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -142,9 +142,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.path.join('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.path.join('EMAIL_HOST_PASSWORD')
 
 
-STRIPE_PUBLIC_KEY = config("STRIPE_PUBLIC_KEY")
-STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY")
+STRIPE_PUBLIC_KEY = os.path.join("STRIPE_PUBLIC_KEY")
+STRIPE_SECRET_KEY = os.path.join("STRIPE_SECRET_KEY")
