48eabaf3a80cf66d5088a1ef5b0045429c682c8e
Marcdonaldtec <marcdonaldtech40@gmail.com>
2024-03-19 14:58:20 -0400

    whitenoise in wsgi for media

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 dotenv_path = BASE_DIR / '.env'
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
--- myportfolio/wsgi.py
+++ myportfolio/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for myportfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
-import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myportfolio.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'media'))
+application.add_files(os.path.join(BASE_DIR, 'media'), prefix='media/')
